8018baaf3056bccee249d39234cebcaf8325bd8b
Miguel Cobian <miguelcobian99@gmail.com>
2024-03-31 21:33:00 -0600

    Feat. Conditional render for food or workout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -71,7 +71,7 @@ export const Form = () => {
             <input
                 type="submit"
                 className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
-                value="Guardar comida o Guardar Ejercicio"
+                value={`${activity.category === 1 ? 'Guardar comida' : 'Guardar ejercicio'} `}
                 disabled={!isValidActivity()}
             />
 
